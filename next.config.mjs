d673946cda086d726004fabad326351ffbce5d5f
nkkumar07 <nkkumawat7357@gmail.com>
2025-06-11 11:33:09 +0530

    Updated files

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 // next.config.mjs
 export default {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    config.cache = false; // Disabling Webpack cache
-    return config;
-  },
 };
